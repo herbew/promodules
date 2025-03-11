9e93796e36cb4080b5238f534c498f0b5cd905d2
herbew <hebrew@gmail.com>
2025-03-11 20:06:19 +0700

    update install requires version

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=[
-        'django>=3.2',
-        'django-formtools>=2.2',
+        'django==5.1.7',
+        'django-formtools==2.5.1',
+        'django-environ==0.12.0',
     ],
     author='Heribertus Rustyawan',
     author_email='herbew@gmail.com',
