a22b2f33bd5faa2669f37969d1e992a2308df7e4
herbew <hebrew@gmail.com>
2025-03-11 10:39:58 +0700

    add setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='promodules',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'Django>=3.2',
+        'django-formtools>=2.2',
+    ],
+    author='Heribertus Rustyawan',
+    author_email='herbew@gmail.com',
+    description='Django Project Management Modules',
+    url='https://github.com/herbew/promodules.git',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
