07312333b82739cbda81e5e3ad06dec33426d782
herbew <hebrew@gmail.com>
2025-03-11 09:45:58 +0700

    add test forms

---
+++ tests/modules/test_forms.py
@@ -0,0 +1,58 @@
+from django.test import TestCase
+from .forms import AddModuleForm, UpdateModuleForm
+from .models import Module
+
+class AddModuleFormTestCase(TestCase):
+    def test_valid_form(self):
+        data = {
+            'name': 'Test Module',
+            'version': '1.0',
+            'status': 'active',
+            'repository': 'https://github.com/herbew/promodules.git',
+            'description': 'This is a test module'
+        }
+        form = AddModuleForm(data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        data = {
+            'name': '',
+            'version': '1.0',
+            'status': 'active',
+            'repository': 'https://github.com/herbew/promodules.git',
+            'description': 'This is a test module'
+        }
+        form = AddModuleForm(data)
+        self.assertFalse(form.is_valid())
+
+class UpdateModuleFormTestCase(TestCase):
+    def setUp(self):
+        self.module = Module.objects.create(
+            name='Test Module',
+            version='1.0',
+            status='active',
+            repository='https://github.com/herbew/promodules.git',
+            description='This is a test module'
+        )
+
+    def test_valid_form(self):
+        data = {
+            'name': 'Updated Test Module',
+            'version': '1.1',
+            'status': 'active',
+            'repository': 'https://github.com/herbew/promodules.git',
+            'description': 'This is an updated test module'
+        }
+        form = UpdateModuleForm(data, instance=self.module)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        data = {
+            'name': '',
+            'version': '1.1',
+            'status': 'active',
+            'repository': 'https://github.com/herbew/promodules.git',
+            'description': 'This is an updated test module'
+        }
+        form = UpdateModuleForm(data, instance=self.module)
+        self.assertFalse(form.is_valid())
--- tests/modules/test_models.py
+++ tests/modules/test_models.py
@@ -7,21 +7,21 @@ class ModuleModelTest(TestCase):
             name='Test Module',
             version='1.0',
             status='installed',
-            repository='https://github.com/herbew/proproducts.git',
-            description='Test Description'
+            repository='https://github.com/herbew/promodules.git',
+            description='This is a test module'
         )
         self.assertEqual(module.name, 'Test Module')
         self.assertEqual(module.version, '1.0')
         self.assertEqual(module.status, 'active')
-        self.assertEqual(module.repository, 'https://github.com/herbew/proproducts.git')
-        self.assertEqual(module.description, 'Test Description')
+        self.assertEqual(module.repository, 'https://github.com/herbew/promodules.git')
+        self.assertEqual(module.description, 'This is a test module')
         
   def test_update_module(self):
         module = Module.objects.create(
             name='Test Module',
             version='1.0',
             status='active',
-            repository='https://github.com/herbew/proproducts.git',
+            repository='https://github.com/herbew/promodules.git',
             description='This is a test module'
         )
         module.name = 'Updated Test Module'
@@ -33,7 +33,7 @@ class ModuleModelTest(TestCase):
             name='Test Module',
             version='1.0',
             status='active',
-            repository='https://github.com/test/module',
+            repository='https://github.com/herbew/promodules.git',
             description='This is a test module'
         )
         module.delete()
