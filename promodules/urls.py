db0d9768dcffa0a1d0ca88619597c1368291214e
herbew <hebrew@gmail.com>
2025-03-12 08:43:30 +0700

    hide the admin urls

--- promodules/urls.py
+++ promodules/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('module/', include('modules.urls',namespace='modules'))
 ]
