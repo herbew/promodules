09eba88efa9a18eee02fbecedca636b3cec5fb05
herbew <hebrew@gmail.com>
2025-03-13 08:41:31 +0700

    add return 403

--- modules/views.py
+++ modules/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import Module
 from .forms import AddModuleForm, UpdateModuleForm
@@ -12,10 +13,23 @@ def synch_module():
             module.save()
     except:
         raise
-
 # Function for check is super user
 def is_superuser(user):
     return user.is_superuser
+    
+def user_is_superuser(view_func):
+    decorated_view_func = user_passes_test(
+        is_superuser,
+        login_url=None,  # If you want to redirect to the login page, fill in the login URL.
+        redirect_field_name=None,
+    )(view_func)
+    
+    def _wrapped_view(request, *args, **kwargs):
+        if not is_superuser(request.user):
+            return HttpResponseForbidden("You do not have permission to access this page.")
+        return decorated_view_func(request, *args, **kwargs)
+    
+    return _wrapped_view
 
 def module_list(request):
     modules = Module.objects.all()
@@ -24,7 +38,7 @@ def module_list(request):
 
 # Only superuser can access this view
 @login_required
-@user_passes_test(is_superuser)
+@user_is_superuser
 def install_module(request):
     if request.method == 'POST':
         form = AddModuleForm(request.POST)
@@ -37,7 +51,7 @@ def install_module(request):
 
 # Only superuser can access this view
 @login_required
-@user_passes_test(is_superuser)
+@user_is_superuser
 def upgrade_module(request, module_id):
     module = get_object_or_404(Module, id=module_id)
     if request.method == 'POST':
@@ -53,7 +67,7 @@ def upgrade_module(request, module_id):
 
 # Only superuser can access this view
 @login_required
-@user_passes_test(is_superuser)
+@user_is_superuser
 def uninstall_module(request, module_id):
     module = get_object_or_404(Module, id=module_id)
     #module.delete()
